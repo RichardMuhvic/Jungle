8724b5d6d6685485ae8936cd631b8d64f3e03257
Richard Muhvic <richardmuhvic@gmail.com>
2022-02-02 20:05:58 +0100

    changement des classes et valeurs

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const scene = document.getElementById('scene');
-const parallaxScene= new Parallax(scene);
+document.addEventListener("mousemove", parallax);
+
+function parallax(e) {
+    document.querySelectorAll('.layer').forEach(function(move) {
+        let movingValue = move.getAttribute('data-valuer');
+
+        let x = (e.clientX * movingValue) / 250;
+        let y = (e.clientY * movingValue) / 250;
+
+        move.style.transform = "translateX("+x+"px) translateY("+y+"px)";
+    });
+}
